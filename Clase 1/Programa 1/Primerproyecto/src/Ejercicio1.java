ddfc6b0b6f5923b83b2ee1fedcb348e48c12b1e7
shyfock <agvanegas@gmail.com>
2021-07-07 10:17:12 -0500

    Adicion de clase Java

---
+++ Clase 1/Programa 1/Primerproyecto/src/Ejercicio1.java
@@ -0,0 +1,24 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Comentarios de línea
+        /* -> abre Comentarios de bloque
+        no importa la cantidad de líneas
+        cierra comentarios -> 
+        */
+        // Las variables se deben declarar (asignar un tipo)
+        int x; //Todas las sentencias terminan en (;)
+        int y;
+        String cad;
+        char m;
+        double a = 3.5;
+        float d = 3.8f; // Para float debe escribirse f al final, sino lo toma como double
+        x = 20;
+        y = x + 1;
+        cad = "Hola";
+        m = 'm';
+        System.out.println(a + d + cad + m + y);
+        //var lista = Lista.of(1, 2, 3);
+
+
+    }
+}
